ac33c3e7fc1e268bfe2554608b11d1719dc70282
gannacon <cwgannaway@gmail.com>
2021-11-10 22:19:01 -0600

    adding navigation

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/NavBar/Navbar";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import { orange } from "@mui/material/colors";
+import { StayPrimaryLandscape } from "@mui/icons-material";
 
 const theme = createTheme({
-  status: {
-    danger: orange[500],
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#FAFAFA",
+    },
+    secondary: {
+      main: "#84FFFF",
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
   },
 });
 
 function firstButton() {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <>
+      {/* <Button variant="contained">Hello World</Button> */}
+      <Nav />
+    </>
+  );
 }
 
 // const renderNav = () => {
@@ -39,9 +59,11 @@ function firstButton() {
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>{firstButton()}</ThemeProvider>
-    </Router>
+    <>
+      <Router>
+        <ThemeProvider theme={theme}>{firstButton()}</ThemeProvider>
+      </Router>
+    </>
   );
 }
 
